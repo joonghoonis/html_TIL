a32e163e33575077610ffc5d20cc7bbf7b1d515e
tlswndgns99 <tlswndgns99@gmail.com>
2025-08-28 18:11:00 +0900

    삼립 풋터 완료

---
+++ figma_layout/samlip/header.php
@@ -0,0 +1,116 @@
+<header>
+    <!-- 로고 -->
+    <a href="index.html"><img src="./images/logo_black.png" alt="SPC삼립"></a>
+    <!-- gnb메뉴 -->
+    <ul class="gnb_menu">
+        <li>
+            <a href="./company.html">COMPANY</a>
+            <ul class="lnb">
+                <li>
+                    <a href="#">SPC삼립 소개</a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">경영이념</a></li>
+                        <li><a href="#">연혁</a></li>
+                        <li><a href="#">사업분야</a></li>
+                        <li><a href="#">공장소개</a></li>
+                        <li><a href="#">주요 관계사</a></li>
+                        <li><a href="#">CI/BI</a></li>
+                        <li><a href="#">오시는 길</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">윤리경영</a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">정도경영</a></li>
+                        <li><a href="#">사회공헌활동</a></li>
+                        <li><a href="#">CCM 인증</a></li>
+                        <li><a href="#">동반성장</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">공정거래</a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">공정거래 자율준수</a></li>
+                        <li><a href="#">CP운영규정</a></li>
+                        <li><a href="#">자율준수편람</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">삼립뉴스<a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">보도자료</a></li>
+                        <li><a href="#">공고</a></li>
+                        <li><a href="#">공지사항</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">안전보건경영<a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">안전보건경영</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">지속가능경영<a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">지속가능경영</a></li>
+                        <li><a href="#">지속가능경영보고서</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li><a href="#">BRANDS</a></li>
+        <li>
+            <a href="#">IR</a>
+            <ul class="lnb">
+                <li>
+                    <a href="#">기업지배구조</a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">이사회</a></li>
+                        <li><a href="#">이사회 내 위원회</a></li>
+                        <li><a href="#">주주총회</a></li>
+                    </ul>
+                </li>  
+                <li>
+                    <a href="#">재무정보</a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">재무상태표</a></li>
+                        <li><a href="#">손익계산서</a></li>
+                        <li><a href="#">현금흐름표</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">주식정보</a>
+                    <ul class="lnb_depth2">
+                        <li><a href="#">공시</a></li>
+                        <li><a href="#">실시간 주가정보</a></li>
+                        <li><a href="#">배당현황</a></li>
+                        <li><a href="#">공시정보관리규정</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li><a href="#">SNS</a></li>
+        <li>
+            <a href="#">RECRUIT</a> 
+            <ul class="lnb">
+                <li><a href="#">인사제도</a></li>
+                <li><a href="#">입사지원</a></li>
+            </ul>
+        </li>
+        <li><a href="#">STORE</a></li>
+    </ul>
+    <!-- right_menu -->
+    <ul class="right_btn">
+        <li><a href="https://spcsamlip.co.kr/customer/">CUSTOMER</a></li>
+        <li>
+            <a href="#"><img src="./images/lang_kr.png" alt="한국어보기"></a>
+            <!-- 마우스 올릴 시 보이는 영역 -->
+                <div class="lang_select">
+                <a href="#" class="kr">KR</a>
+                <a href="#" class="en">EN</a>
+                <a href="#" class="ch">CH</a>
+                </div>
+        </li>
+        <li><a href="#"><img src="./images/search.png" alt="검색하기"></a></li>
+    </ul>
+</header>
