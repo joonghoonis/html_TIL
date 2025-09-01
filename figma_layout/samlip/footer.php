b6e986613ea69867794386af7572f27d93d8cd77
tlswndgns99 <tlswndgns99@gmail.com>
2025-09-01 18:12:35 +0900

    반응형 웹 연습

---
+++ figma_layout/samlip/footer.php
@@ -0,0 +1,42 @@
+<footer>
+            <div class="ft_top">
+                    <div class="menu">
+                    <a href="#">개인정보처리방침</a>
+                    <a href="#">영상정보처리기기 운영관리방침</a>
+                    <a href="#">거래희망회사 사전등록</a>
+                    <a href="#">동반성장</a>
+                    <a href="#"><img src="./images/ft_ccm.png" alt=""></a>
+                </div>
+                <ul class="site_all">
+                    <li><p>FAMILY SITES</p>
+                        <ul class="family_list">
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                            <li><a href="#">PALIS BAGUETEE</a></li>
+                        </ul>
+                    </li>
+                    <li><p>SITE MAP</p>
+                        <div class="site_wrap">
+                            <div class="sitemap_contents"><!-- 배경 Bg -->
+                                <h2>Sitemap</h2>
+                                <button type="button" class="close"></button>
+                                <!-- 사이트맵 목록 작성위치 -->
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="ft_btn">
+                <address><em>고객센터</em> 전화번호 080-739-8572(월~금 09:00~17:00)<br>
+                    101, Gongdan 1-daero, Siheung-si, Gyeonggi-do, Republic of Korea ㈜SPC삼립 대표이사 황종현
+                </address>
+                <p>&copy; SPC Samlip. All rights reserved.</p>
+            </div>
+        </footer>
